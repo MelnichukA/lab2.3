fe7e236d9f3627d094e76d46e089a4808cb3d88d
1710517 <1710517@L2-24>
2018-02-16 11:34:18 +0200

    Add project files.

---
+++ lab2.3/lab2.3.cpp
@@ -0,0 +1,27 @@
+// lab2.3.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+	double a = -3, b = 3, c =1, d = 2, hx = 0.25, hy = 0.15;
+	printf("%c\\%c", 'Y', 'X');
+	for (double x = a; x <= b; x += hx) {
+		printf("\t%8.2f", x);
+	}
+	printf("\n");
+	for (double y = c; y <= d; y += hy) {
+		printf("%.2f", y);
+		for (double x = a; x <= b; x += hx) {
+			double z = sqrt(x*(y+1))/(x - y);
+			printf("\t%8.2f", z);
+		}
+		printf("\n");
+	}
+	printf("\n");
+	getchar();
+    return 0;
+}
+
